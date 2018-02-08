aaf5ba266571f6365d6dfa874f77d1ec30b96fe1
equus <kirill.kuzn1998@yandex.ru>
2018-02-08 15:53:14 +0300

    my

---
+++ Goblin/Models/MainContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
+
+namespace Goblin.Models
+{
+    public class MainContext : DbContext
+    {
+        public DbSet<Person> Persones { get; set; }
+
+        public MainContext(DbContextOptions<MainContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Goblin/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace Goblin.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public int VkID { get; set; }
+        public short GroupID { get; set; }
+        public bool Schedule { get; set; }
+        public bool Weather { get; set; }
+        public string City { get; set; }
+        public int CityID { get; set; }
+    }
+}
