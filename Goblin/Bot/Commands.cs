60300fc396a3ffd1862f038d793b154020685068
equus <kirill.kuzn1998@yandex.ru>
2018-02-08 19:02:52 +0300

    add help command

--- Goblin/Bot/Commands.cs
+++ Goblin/Bot/Commands.cs
@@ -7,11 +7,12 @@ namespace Goblin.Bot
     {
         private static List<ICommand> Commands = new List<ICommand>()
         {
-            new RandomCommand()
+            new RandomCommand(),
         };
 
         public static string ExecuteCommand(string message)
         {
+            Commands.Add(new HelpCommand(Commands));
             var split = message.Split(' ', 2);
             var comm = split[0];
             var param = split.Length > 1 ? split[1] : "";
---
+++ Goblin/Bot/Commands/HelpCommand.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Goblin.Bot.Commands
+{
+    public class HelpCommand : ICommand
+    {
+        public string Name { get; } = "Команды";
+        public string Decription { get; } = "описание команд";
+        public List<string> Allias { get; } = new List<string>() {"help", "команды"};
+        public Category Category { get; } = Category.Common;
+        public bool IsAdmin { get; } = false;
+        public string Result { get; set; }
+
+        public HelpCommand(List<ICommand> cmds)
+        {
+            var common = "Общие команды:\n";
+            var safu = "Команды для САФУ:\n";
+            foreach (var cmd in cmds.Where(x => x.Category == Category.Common))
+            {
+                common += $"{cmd.Name} - {cmd.Decription}\n";
+            }
+            foreach (var cmd in cmds.Where(x => x.Category == Category.SAFU))
+            {
+                safu += $"{cmd.Name} - {cmd.Decription}\n";
+            }
+
+            Result = $"Общее число команд на данный момент: {cmds.Count + 1}\n\n{common}\n\n{safu}";
+        }
+
+        public void Execute(string param)
+        {
+            
+        }
+    }
+}
