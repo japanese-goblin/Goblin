1fa46f935d094130ec6ccf9a91f25f9510e5e38d
equus <kirill.kuzn1998@yandex.ru>
2018-02-08 16:02:44 +0300

    Migration

---
+++ Goblin/Persons.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Goblin
+{
+    public partial class Persons
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public int CityId { get; set; }
+        public short GroupId { get; set; }
+        public bool Schedule { get; set; }
+        public bool Weather { get; set; }
+        public int VkId { get; set; }
+    }
+}
