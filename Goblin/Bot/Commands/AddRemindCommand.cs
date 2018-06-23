098613a6f6f644e18c63422fc3e7ef39fff7e4e2
equus <kirill.kuzn1998@yandex.ru>
2018-06-23 19:15:12 +0300

    fix? reminds

--- Goblin/Bot/Commands/AddRemindCommand.cs
+++ Goblin/Bot/Commands/AddRemindCommand.cs
@@ -8,9 +8,9 @@ namespace Goblin.Bot.Commands
 {
     public class AddRemindCommand : ICommand
     {
-        public string Name { get; } = "Напомни *день*.*месяц*.*год* *час* *текст*";
+        public string Name { get; } = "Напомни *день*.*месяц*.*год* *час*:*минута* *текст*";
         public string Decription { get; } = "Напоминает в указанное время о каком-то очень ВАЖНОМ тексте. День и месяц обязательно должны содержать 2 цифры, а год - 4. В указанное время бот напишет в личку сообщение с заданным текстом.";
-        public string Usage { get; } = "Напомни 01.02.2018 15 зачет";
+        public string Usage { get; } = "Напомни 01.02.2018 15:35 зачет";
         public List<string> Allias { get; } = new List<string>() {"напомни"};
         public Category Category { get; } = Category.Common;
         public bool IsAdmin { get; } = false;
@@ -30,7 +30,7 @@ namespace Goblin.Bot.Commands
                 Result = "Превышен лимит (8) напоминалок";
                 return;
             }
-            var time = DateTime.ParseExact($"{all[0]} {all[1]}", "dd.MM.yyyy HH", CultureInfo.InvariantCulture);
+            var time = DateTime.ParseExact($"{all[0]} {all[1]}", "dd.MM.yyyy HH:mm", CultureInfo.InvariantCulture);
             var addedTime = time.AddHours(-3);
             if (addedTime < DateTime.Now)
             {
