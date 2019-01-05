87ac28f0eb201cde76859eb16e02e653d18dcd66
equus <kirill.kuzn98@gmail.com>
2019-01-05 19:14:17 +0300

    create partial views for admin panel

--- Goblin/ScheduledTasks.cs
+++ Goblin/ScheduledTasks.cs
@@ -1,11 +1,11 @@
 ﻿using FluentScheduler;
 using Goblin.Helpers;
-using System;
-using System.Linq;
-using System.Threading.Tasks;
 using Goblin.Schedule;
 using Goblin.Vk;
 using Goblin.Weather;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Goblin
 {
@@ -17,14 +17,18 @@ namespace Goblin
             //Schedule(async () => await SendSchedule()).ToRunEvery(0).Days().At(6, 0);
             //Schedule(async () => await SendWeather()).ToRunEvery(0).Days().At(7, 0);
             //Schedule(async () => await SendToConv(5, 351616)).ToRunEvery(0).Days().At(6, 05); // IGOR
-            Schedule(async () => await SendToConv(2, 351517)).ToRunEvery(0).Days().At(6, 15); // MY
+            Schedule(async () => await SendToConv(2, 351617)).ToRunEvery(0).Days().At(6, 15); // MY
         }
 
         private async Task SendRemind()
         {
             //TODO: ?????
             var reminds = DbHelper.Db.Reminds.Where(x => $"{x.Date:dd.MM.yyyy HH:mm}" == $"{DateTime.Now:dd.MM.yyyy HH:mm}");
-            if (!reminds.Any()) return;
+            if (!reminds.Any())
+            {
+                return;
+            }
+
             foreach (var remind in reminds)
             {
                 await VkMethods.SendMessage(remind.VkID, $"Напоминаю:\n {remind.Text}");
@@ -36,7 +40,11 @@ namespace Goblin
 
         private async Task SendSchedule()
         {
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday) return;
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                return;
+            }
+
             await Task.Factory.StartNew(async () =>
             {
                 var grouped = DbHelper.GetScheduleUsers().GroupBy(x => x.Group);
@@ -66,7 +74,10 @@ namespace Goblin
 
         private async Task SendToConv(int id, int group = 0, string city = "")
         {
-            if (!StudentsSchedule.IsCorrectGroup(group)) return;
+            if (!StudentsSchedule.IsCorrectGroup(group))
+            {
+                return;
+            }
 
             id = 2000000000 + id;
 
