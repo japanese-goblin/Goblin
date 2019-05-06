7ecc63e42042dae49f0c8ba528ba72d1da715fe8
equus <kirill.kuzn98@gmail.com>
2019-05-06 22:58:08 +0300

    Добавить таблицу с юзерами в админке

--- Goblin.WebUI/Controllers/AdminController.cs
+++ Goblin.WebUI/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Linq;
+using Goblin.Persistence;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Goblin.WebUI.Controllers
@@ -6,7 +8,18 @@ namespace Goblin.WebUI.Controllers
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
+        {
+            return View(_context.GetUsers().OrderBy(x => x.Id).ToList());
+        }
+        public IActionResult Messages()
         {
             return View();
         }
