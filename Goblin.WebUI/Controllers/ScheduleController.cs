31e15b1f5200a7f62dcb790a331ad81b72034eef
equus <kirill.kuzn98@gmail.com>
2019-05-06 21:07:14 +0300

    Добавить расписание

---
+++ Goblin.WebUI/Controllers/ScheduleController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Narfu;
+
+namespace Goblin.WebUI.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Show(int id)
+        {
+            if(!ModelState.IsValid || !StudentsSchedule.IsCorrectGroup(id))
+            {
+                HttpContext.Response.StatusCode = 404;
+                return View("Error");
+            }
+
+            var group = StudentsSchedule.GetGroupByRealId(id);
+            var (error, _, lessons) = await StudentsSchedule.GetSchedule(id);
+
+            if(error)
+            {
+                HttpContext.Response.StatusCode = 503;
+                return View("Error");
+            }
+
+            ViewBag.Title = $"{group.RealId} - {group.Name}";
+            var result = lessons.GroupBy(x => StudentsSchedule.GetWeekNumber(x.StartTime))
+                                .ToDictionary(x => $"{x.First().StartTime:dd.MM.yyyy} - {x.Last().StartTime:dd.MM.yyyy}",
+                                              x => x.ToArray()); // TODO: fix key
+            return View(result);
+        }
+    }
+}
---
+++ Goblin.WebUI/TagHelpers/LessonTagHelper.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Narfu.Models;
+
+namespace Goblin.WebUI.TagHelpers
+{
+    public class LessonTagHelper : TagHelper
+    {
+        public Lesson Lesson { get; set; }
+        private string _cssStyle;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            GetStyleForLessonType();
+            output.Content.SetHtmlContent($@"<div class='timetable_sheet {_cssStyle}'>
+<span class='time_para'>{Lesson.StartEndTime}</span>
+<span class='kindOfWork'>{Lesson.Type}</span>
+<span class='discipline'>{Lesson.Name} ({Lesson.Teacher})</span>
+<span class='group'>{Lesson.Groups}</span>
+<span class='auditorium'><b>ауд. {Lesson.Auditory}</b> {Lesson.Address}</span>
+</div>");
+        }
+
+        private void GetStyleForLessonType()
+        {
+            if(Lesson.Type == "Лекции")
+            {
+                _cssStyle = "green";
+            }
+            else if(Lesson.Type == "Лабораторные занятия")
+            {
+                _cssStyle = "blue";
+            }
+            else if(Lesson.Type == "Практические занятия")
+            {
+                _cssStyle = "yellow";
+            }
+            else if(Lesson.Type == "Консультация")
+            {
+                _cssStyle = "orange";
+            }
+            else if(Lesson.Type.ToLower().Contains("экзамен") || Lesson.Type.ToLower().Contains("зачет"))
+            {
+                _cssStyle = "red";
+            }
+        }
+    }
+}
