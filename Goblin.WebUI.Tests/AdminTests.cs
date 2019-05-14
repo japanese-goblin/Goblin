75871d4f7372832797d192c282f2f8b8e5d927cb
equus <kirill.kuzn98@gmail.com>
2019-05-14 15:23:14 +0300

    Добавить тесты для WebUI

---
+++ Goblin.WebUI.Tests/AdminTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Goblin.WebUI.Tests
+{
+    public class AdminTests :  IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public AdminTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact(DisplayName = "Check authorization")]
+        public async void CheckAuthorize()
+        {
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/Admin");
+
+            Assert.StartsWith("https://localhost/Identity/Account/Login", 
+                              response.RequestMessage.RequestUri.OriginalString);
+        }
+    }
+}
---
+++ Goblin.WebUI.Tests/HomeTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Goblin.WebUI.Tests
+{
+    public class HomeTests :  IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public HomeTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact(DisplayName = "Get home page")]
+        public async void Index()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
