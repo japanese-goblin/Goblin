d67f055ceac468d0fbd136d08f78ab782196b997
equus <kirill.kuzn98@gmail.com>
2019-05-18 18:13:22 +0300

    Убрать удаление пользователя при отписке от группы

--- Goblin.Bot/Notifications/GroupLeave/GroupLeaveNotificationHandler.cs
+++ Goblin.Bot/Notifications/GroupLeave/GroupLeaveNotificationHandler.cs
@@ -1,9 +1,7 @@
-﻿using System.Linq;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using Goblin.Persistence;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 using Vk;
 
 namespace Goblin.Bot.Notifications.GroupLeave
@@ -22,16 +20,8 @@ namespace Goblin.Bot.Notifications.GroupLeave
         public async Task Handle(GroupLeaveNotification notification, CancellationToken cancellationToken)
         {
             var userId = Vk.Models.Responses.GroupLeave.FromJson(notification.Response.Object.ToString()).UserId;
-            var botUser = await _context.BotUsers.FirstOrDefaultAsync(x => x.Vk == userId,
-                                                             cancellationToken);
-
-            if(botUser != null)
-            {
-                _context.BotUsers.Remove(botUser);
-                await _context.SaveChangesAsync(cancellationToken);
-            }
-
             var vkUser = await _api.Users.Get(userId);
+
             await _api.Messages.Send(_context.GetAdmins(), $"@id{userId} ({vkUser}) отписался");
         }
     }
