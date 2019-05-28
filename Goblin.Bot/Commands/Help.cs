ff9850ddbca613d2eab83731d4f74b3083e9d0a8
equus <kirill.kuzn98@gmail.com>
2019-05-28 12:49:32 +0300

    Удалить старый код из команды Help

--- Goblin.Bot/Commands/Help.cs
+++ Goblin.Bot/Commands/Help.cs
@@ -67,37 +67,3 @@ namespace Goblin.Bot.Commands
         }
     }
 }
-
-/*
-    private static string _message = "";
-
-    public Help(IEnumerable<ICommand> cmds)
-    {
-        if(!string.IsNullOrEmpty(_message)) return;
-
-        cmds = cmds.Where(x => !x.IsAdmin).OrderBy(x => x.Name).ToList();
-        //TODO: edit it (GroupBy?)
-        var common = "Общие команды:\n";
-        byte com = 1;
-        var safu = "Команды для САФУ:\n";
-        byte saf = 1;
-        foreach(var cmd in cmds.Where(x => x.Category == Category.Common && !x.IsAdmin))
-        {
-            common += $"{com++}) {cmd.Name} - {cmd.Decription}\nНапример - {cmd.Usage}\n";
-        }
-
-        foreach(var cmd in cmds.Where(x => x.Category == Category.SAFU && !x.IsAdmin))
-        {
-            safu += $"{saf++}) {cmd.Name} - {cmd.Decription}\nНапример - {cmd.Usage}\n";
-        }
-
-        _message = $"Общее число команд на данный момент: {cmds.Count(x => !x.IsAdmin)}\n\n" +
-                   $"{common}\n\n" +
-                   $"{safu}\n\n" +
-                   "Для просмотра расписания преподавателя, необходимо получить его номер через команду ('найти деменков'). " +
-                   "После каждого ФИО и названия кафедры указан пятизначный номер преподавателя, который можно использовать для " +
-                   "получения расписания у препода (найти 23512).\n" +
-                   // "Если Вам нужно посмотреть расписание до конца семестра (а не на месяц, как на официальном сайте), то можете зайти сюда (https://equus-cs.herokuapp.com/Schedule), ввести свой номер группы и посмотреть расписание!!\n\n" +
-                   "По любым вопросам/предложениям/ошибкам и прочему, прошу писать сюда: @id***REMOVED*** (тык)";
-    }
- */
