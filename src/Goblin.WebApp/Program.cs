cb905a5148e296d15d7fc18a7e5f311e224f7729
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2019-08-07 00:26:48 +0300

    Добавить веб проект

---
+++ src/Goblin.WebApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Goblin.DataAccess;
+using Goblin.WebApp.Extensions;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Goblin.WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build()
+                                      .InitializeDatabase<IdentityUsersDbContext>()
+                                      .InitializeDatabase<BotDbContext>()
+                                      .Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+                WebHost.CreateDefaultBuilder(args)
+                       .UseStartup<Startup>();
+    }
+}
---
+++ src/Goblin.WebApp/Startup.cs
@@ -0,0 +1,79 @@
+﻿using Goblin.Application;
+using Goblin.Application.Abstractions;
+using Goblin.Application.Commands;
+using Goblin.DataAccess;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VkNet;
+using VkNet.Abstractions;
+using VkNet.Model;
+
+namespace Goblin.WebApp
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; }
+        
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<BotDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddDbContext<IdentityUsersDbContext>(options => options.UseSqlServer(connectionString));
+
+            services.AddSingleton<IVkApi, VkApi>(x =>
+            {
+                var token = Configuration["Vk:AccessToken"];
+                var api = new VkApi();
+                api.Authorize(new ApiAuthParams
+                {
+                    AccessToken = token
+                });
+                return api;
+            });
+
+            services.AddScoped<IBotCommand, RandomCommand>();
+            services.AddScoped<CommandsService>();
+            services.AddScoped<CallbackHandler>();
+            
+            services.AddDefaultIdentity<IdentityUser>()
+                    .AddDefaultUI(UIFramework.Bootstrap4)
+                    .AddEntityFrameworkStores<IdentityUsersDbContext>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
