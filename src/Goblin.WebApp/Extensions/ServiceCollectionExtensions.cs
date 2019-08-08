85c42ac4adb2ac37a444082d8d24d5f0cbcfe4da
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2019-08-08 20:14:23 +0300

    Вынести добавление сервисов в extensions

---
+++ src/Goblin.WebApp/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,45 @@
+using Goblin.Application;
+using Goblin.Application.Abstractions;
+using Goblin.Application.Commands;
+using Goblin.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VkNet;
+using VkNet.Abstractions;
+using VkNet.Model;
+
+namespace Goblin.WebApp.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddVkApi(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IVkApi, VkApi>(x =>
+            {
+                var token = configuration["Vk:AccessToken"];
+                var api = new VkApi();
+                api.Authorize(new ApiAuthParams
+                {
+                    AccessToken = token
+                });
+                return api;
+            });
+        }
+
+        public static void AddDbContexts(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<BotDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddDbContext<IdentityUsersDbContext>(options => options.UseSqlServer(connectionString));
+        }
+
+        public static void AddBotFeatures(this IServiceCollection services)
+        {
+            services.AddScoped<IBotCommand, DebugCommand>();
+            services.AddScoped<CommandsService>();
+            services.AddScoped<CallbackHandler>();
+        }
+    }
+}
