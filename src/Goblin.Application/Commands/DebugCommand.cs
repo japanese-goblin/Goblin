c8178a80a09d54997b504d74947e9f32ee3e2d97
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2019-08-08 20:14:04 +0300

    Добавить команду Debug

---
+++ src/Goblin.Application/Commands/DebugCommand.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Goblin.Application.Abstractions;
+using Goblin.Application.Results;
+using Goblin.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using VkNet.Model;
+
+namespace Goblin.Application.Commands
+{
+    public class DebugCommand : IBotCommand
+    {
+        public string Name => "Дебаг";
+        public string Description => "Получить информацию о состоянии бота";
+        public string Usage => Name;
+        public bool IsAdminCommand => true;
+        public string[] Aliases => new[] { "дебуг", "дебаг" };
+        
+        private readonly BotDbContext _db;
+
+        public DebugCommand(BotDbContext db)
+        {
+            _db = db;
+        }
+        
+        public Task<IResult> Execute(Message msg)
+        {
+            var strBuilder = new StringBuilder();
+            
+            var bday = new DateTime(2017, 4, 29, 19, 42, 0);
+            var dis = DateTime.Now - bday;
+
+            var startTime = Process.GetCurrentProcess().StartTime;
+            var uptime = DateTime.Now - startTime;
+
+            var users = _db.BotUsers.AsNoTracking().Count();
+            var subscribes = _db.Subscribes.AsNoTracking();
+            
+            strBuilder.AppendFormat("Время старта: {0:F}", startTime).AppendLine();
+            strBuilder.AppendFormat("Я работаю уже {0} часов {1} минут", uptime.Hours, uptime.Minutes)
+                      .AppendLine();
+            strBuilder.AppendFormat("Мне уже {0} дней ({1:dd.MM.yyyy})", dis.Days, bday)
+                      .AppendLine().AppendLine();
+            strBuilder.AppendFormat("Всего пользователей {0} ({1} расписание и {2} погода)",
+                                    users, subscribes.Count(x => x.IsSchedule),
+                                    subscribes.Count(x => x.IsWeather));
+
+            return Task.FromResult<IResult>(new SuccessfulResult
+            {
+                Message = strBuilder.ToString()
+            });
+        }
+
+        public Task<IResult> CanExecute(Message msg)
+        {
+            return Task.FromResult<IResult>(new SuccessfulResult());
+        }
+    }
+}
