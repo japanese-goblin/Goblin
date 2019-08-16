05cc4ad793f5a47dfef2983e94d8bf6e6f6344c4
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2019-08-16 20:55:51 +0300

    Добавить команду-заглушку для погоды на день Добавить метод для добавление кнопки возврата в главное меню

--- src/Goblin.Application/KeyboardCommands/WeatherKeyboardCommand.cs
+++ src/Goblin.Application/KeyboardCommands/WeatherKeyboardCommand.cs
@@ -10,26 +10,25 @@ using VkNet.Model.Keyboard;
 
 namespace Goblin.Application.KeyboardCommands
 {
-    public class ScheduleKeyboardCommand : IKeyboardCommand
+    public class WeatherKeyboardCommand : IKeyboardCommand
     {
-        public string Trigger => "scheduleKeyboard";
+        public string Trigger => "weatherKeyboard";
         
         public Task<IResult> Execute(Message msg, BotUser user = null)
         {
             const string DefaultFormat = "dd.MM.yyyy";
             var date = DateTime.Now;
-            
             var kb = new KeyboardBuilder(true);
-            kb.AddButton("На сегодня", "schedule",
+            kb.AddButton("На сегодня", "weather",
                          KeyboardButtonColor.Primary, date.ToString(DefaultFormat));
             kb.AddLine();
-            kb.AddButton("На завтра", "schedule",
+            kb.AddButton("На завтра", "weather",
                          KeyboardButtonColor.Primary, date.AddDays(1).ToString(DefaultFormat));
             kb.AddReturnToMenuButton();
-            
+
             return Task.FromResult<IResult>(new SuccessfulResult
             {
-                Message = "Выберите день",
+                Message = "Выберите день для получения погоды:",
                 Keyboard = kb.Build()
             });
         }
