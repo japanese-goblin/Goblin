f6791a3c49f567c7672e5084f25409f383ae548c
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2019-08-18 18:13:31 +0300

    Добавить команду SendToAdmin

---
+++ src/Goblin.Application/Commands/Text/SendToAdminCommand.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Goblin.Application.Abstractions;
+using Goblin.Application.Extensions;
+using Goblin.Application.Results;
+using Goblin.DataAccess;
+using Goblin.Domain.Entities;
+using VkNet.Abstractions;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+
+namespace Goblin.Application.Commands.Text
+{
+    public class SendToAdminCommand : ITextCommand
+    {
+        public bool IsAdminCommand => false;
+        public string[] Aliases => new[] { "админ", "сообщение" };
+        
+        private readonly IVkApi _api;
+        private readonly BotDbContext _db;
+
+        public SendToAdminCommand(IVkApi api, BotDbContext db)
+        {
+            _api = api;
+            _db = db;
+        }
+
+        public async Task<IResult> Execute(Message msg, BotUser user)
+        {
+            var param = msg.GetCommandParameters();
+            if(param.Length < 2)
+            {
+                return new FailedResult("Введите текст сообщения.");
+            }
+
+            var msgText = $"Сообщение от @id{msg.FromId}:\n{string.Join(' ', param)}";
+
+            await _api.Messages.SendToUserIdsAsync(new MessagesSendParams
+            {
+                UserIds = _db.BotUsers.Where(x => x.IsAdmin).Select(x => x.VkId),
+                Message = msgText,
+                RandomId = new Random().Next(0, int.MaxValue)
+            });
+            return new SuccessfulResult
+            {
+                Message = "Ваше сообщение успешно отправлено."
+            };
+        }
+    }
+}
