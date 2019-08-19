88270cf4de118571466fedbdbc09140c81967d64
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2019-08-19 14:06:16 +0300

    Исправить отправку Confirmation code

--- src/Goblin.WebApp/Controllers/VkCallbackController.cs
+++ src/Goblin.WebApp/Controllers/VkCallbackController.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Goblin.Application;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json.Linq;
 using VkNet.Utils;
 
@@ -11,21 +12,23 @@ namespace Goblin.WebApp.Controllers
     public class VkCallbackController : ControllerBase
     {
         private readonly CallbackHandler _handler;
+        private readonly IConfiguration _config;
 
-        public VkCallbackController(CallbackHandler handler)
+        public VkCallbackController(CallbackHandler handler, IConfiguration config)
         {
             _handler = handler;
+            _config = config;
         }
 
         [HttpPost]
         public async Task<string> Handle([FromBody] object update)
         {
-            // if(update.Type.ToString() == "confirmation")
-            // {
-            //     //TODO:
-            //     return "Confirmation code";
-            // }
-            //
+            var temp = update as dynamic; //TODO: пофиксить в следующей версии VkNet
+            if(temp["type"] == "confirmation")
+            {
+                //TODO:
+                return _config["Vk:ConfirmationCode"];
+            }
             return await _handler.Handle(new VkResponse(JToken.FromObject(update)));
         }
     }
