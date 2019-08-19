c0c82c1fa6be3519d2f3ea38c650b3e9f5bdf935
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2019-08-19 11:37:14 +0300

    Добавить админ-панель

---
+++ src/Goblin.WebApp/Controllers/AdminController.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Goblin.Application.Extensions;
+using Goblin.DataAccess;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VkNet.Abstractions;
+using VkNet.Model.RequestParams;
+
+namespace Goblin.WebApp.Controllers
+{
+    [Authorize] //TODO: roles
+    public class AdminController : Controller
+    {
+        private readonly BotDbContext _db;
+        private readonly IVkApi _vkApi;
+
+        public AdminController(BotDbContext db, IVkApi vkApi)
+        {
+            _db = db;
+            _vkApi = vkApi;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.BotUsers.Include(x => x.SubscribeInfo).ToArray());
+        }
+        
+        public IActionResult Messages()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendToAll(string msg, string[] attachs)
+        {
+            var gr = _db.BotUsers.Select(x => x.VkId).ToArray();
+            await _vkApi.Messages.SendToUserIdsAsync(new MessagesSendParams
+            {
+                Message = msg,
+                UserIds = gr,
+                RandomId = new Random().Next(0, int.MaxValue)
+            });
+            return RedirectToAction("Messages", "Admin");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendToId(long peerId, string msg, string[] attachs)
+        {
+            await _vkApi.Messages.SendWithRandomId(new MessagesSendParams
+            {
+                PeerId = peerId,
+                Message = msg
+            });
+            return RedirectToAction("Messages", "Admin");
+        }
+    }
+}
