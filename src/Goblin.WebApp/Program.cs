1d1c414e375dd5a094c4156fe0ec77ff49841ad2
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2019-08-21 17:44:51 +0300

    Добавить установку дефолтной локали

--- src/Goblin.WebApp/Program.cs
+++ src/Goblin.WebApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using Goblin.DataAccess;
+﻿using System.Globalization;
+using Goblin.DataAccess;
 using Goblin.WebApp.Extensions;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -9,6 +10,7 @@ namespace Goblin.WebApp
     {
         public static void Main(string[] args)
         {
+            SetDefaultLocale();
             CreateWebHostBuilder(args).Build()
                                       .InitializeDatabase<IdentityUsersDbContext>()
                                       .InitializeDatabase<BotDbContext>()
@@ -19,6 +21,15 @@ namespace Goblin.WebApp
         {
             return WebHost.CreateDefaultBuilder(args)
                           .UseStartup<Startup>();
+        }
+
+        private static void SetDefaultLocale()
+        {
+            var culture = new CultureInfo("ru-RU");
+            CultureInfo.CurrentCulture = culture;
+            CultureInfo.CurrentUICulture = culture;
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
         }
     }
 }
