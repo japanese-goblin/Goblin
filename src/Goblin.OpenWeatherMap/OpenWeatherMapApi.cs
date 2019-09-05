3cbf35a3b65e6cbb2e837185d42921e25fc5d1c4
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2019-09-05 18:09:17 +0300

    Добавить логи в Goblin.OpenWeatherMap

--- src/Goblin.OpenWeatherMap/OpenWeatherMapApi.cs
+++ src/Goblin.OpenWeatherMap/OpenWeatherMapApi.cs
@@ -4,39 +4,50 @@ using System.Threading.Tasks;
 using Flurl.Http;
 using Goblin.OpenWeatherMap.Models.Current;
 using Goblin.OpenWeatherMap.Models.Daily;
+using Serilog;
 
 namespace Goblin.OpenWeatherMap
 {
     public class OpenWeatherMapApi
     {
         private readonly string _token;
+        private readonly ILogger _logger;
 
         public OpenWeatherMapApi(string token)
         {
+            _logger = Log.ForContext<OpenWeatherMapApi>();
+            
+            _logger.Debug("Инициализация {0}", nameof(OpenWeatherMapApi));
             if(string.IsNullOrWhiteSpace(token))
             {
+                _logger.Fatal("Токен пуст", nameof(OpenWeatherMapApi));
                 throw new ArgumentException("Токен пуст");
             }
-
+            
             _token = token;
         }
 
         public async Task<CurrentWeather> GetCurrentWeather(string city)
         {
+            _logger.Debug("Получение погоды на текущий момент в городе {0}", city);
             var response = await Defaults.BuildRequest(_token)
                                          .AppendPathSegment("weather")
                                          .SetQueryParam("q", city)
                                          .GetJsonAsync<CurrentWeather>();
+            _logger.Debug("Погода получена");
+            
             return response;
         }
 
         public async Task<DailyWeatherListItem> GetDailyWeatherAt(string city, DateTime date)
         {
+            _logger.Debug("Получение погоды на день в городе {0} на дату {1:dd.MM.yyyy}", city, date);
             var dif = date.Date - DateTime.Today;
             if(dif > TimeSpan.FromDays(Defaults.MaxDailyWeatherDifference))
             {
-                throw new
-                        ArgumentException($"Погоду можно получить максимум на {Defaults.MaxDailyWeatherDifference} дней");
+                var msg = $"Погоду можно получить максимум на {Defaults.MaxDailyWeatherDifference} дней";
+                _logger.Error(msg);
+                throw new ArgumentException(msg );
             }
 
             var count = dif.Days + 1;
@@ -49,18 +60,22 @@ namespace Goblin.OpenWeatherMap
             var weather = response.List.FirstOrDefault(x => Defaults.UnixToDateTime(x.UnixTime).Date == date.Date);
             if(weather is null)
             {
-                throw new ArgumentException("Погода на указанную дату не найдена.");
+                var msg = $"Погода на {date:dd.MM.yyyy} не найдена.";
+                _logger.Error(msg);
+                throw new ArgumentException(msg);
             }
+            _logger.Debug("Погода получена");
 
             return weather;
         }
 
         public async Task<bool> IsCityExists(string city)
         {
+            _logger.Debug("Проверка на существование города {0}", city);
             var response = await Defaults.BuildRequest(_token)
                                          .AppendPathSegment("weather")
                                          .SetQueryParam("q", city)
-                                         .GetAsync();
+                                         .HeadAsync();
 
             return response.IsSuccessStatusCode;
         }
