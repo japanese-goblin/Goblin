c4af8e8dbf455d4b02677fc5285026a3766a6da2
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2019-09-08 12:48:32 +0300

    Исправить время рассылки погоды

--- src/Goblin.WebApp/Extensions/ApplicationBuilderExtensions.cs
+++ src/Goblin.WebApp/Extensions/ApplicationBuilderExtensions.cs
@@ -30,7 +30,7 @@ namespace Goblin.WebApp.Extensions
             RecurringJob.AddOrUpdate<ScheduleTask>("SendDailySchedule", x => x.SendSchedule(),
                                                    "05 6 * * 1-6", TimeZoneInfo.Local);
             RecurringJob.AddOrUpdate<WeatherTask>("SendDailyWeather", x => x.SendDailyWeather(),
-                                                  "25 5 * * *", TimeZoneInfo.Local);
+                                                  "30 6 * * *", TimeZoneInfo.Local);
         }
     }
 }
