6b7a2ade3e43cd604b11359fa16b0291a6330170
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2019-09-11 11:24:01 +0300

    Убрать лишние скобки в расписании преподавателя

--- src/Goblin.Narfu/Models/TeacherLessonsViewModel.cs
+++ src/Goblin.Narfu/Models/TeacherLessonsViewModel.cs
@@ -21,8 +21,8 @@ namespace Goblin.Narfu.Models
                 strBuilder.AppendFormat("{0:D}", group.Key).AppendLine();
                 foreach(var lesson in group)
                 {
-                    strBuilder.AppendFormat("{0:t} - {1} [{2}] ({3})",
-                                            lesson.StartTime, lesson.Name, lesson.Type, lesson.Teacher)
+                    strBuilder.AppendFormat("{0:t} - {1} [{2}]",
+                                            lesson.StartTime, lesson.Name, lesson.Type)
                               .AppendLine()
                               .AppendFormat("У группы {0}", lesson.Groups).AppendLine()
                               .AppendFormat("В {0} ({1})", lesson.Auditory, lesson.Address).AppendLine()
