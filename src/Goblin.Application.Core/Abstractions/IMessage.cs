131d85364ad3784cf073b96e119b5459b6ab90b0
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-18 22:19:40 +0300

    Исправить обработку команд и параметров

--- src/Goblin.Application.Core/Abstractions/IMessage.cs
+++ src/Goblin.Application.Core/Abstractions/IMessage.cs
@@ -9,7 +9,7 @@ namespace Goblin.Application.Core.Abstractions
         public string Text { get; set; }
         public string Payload { get; set; }
 
-        public string[] MessageParams => Text.Split(' ').Skip(1).ToArray();
-        public string CommandName => Text.Split(' ').FirstOrDefault();
+        public string[] MessageParams => Text.ToLower().Split(' ').Skip(1).ToArray();
+        public string CommandName => Text.ToLower().Split(' ').FirstOrDefault();
     }
 }
