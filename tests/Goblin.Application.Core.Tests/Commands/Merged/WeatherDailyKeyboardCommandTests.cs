5fb10633e19a3868b59a29b75530ea135039e077
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-25 10:40:59 +0300

    Добавить тесты для WeatherDailyKeyboardCommand

---
+++ tests/Goblin.Application.Core.Tests/Commands/Merged/WeatherDailyKeyboardCommandTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Goblin.Application.Core.Commands.Merged;
+using Goblin.Application.Core.Results.Failed;
+using Goblin.Application.Core.Results.Success;
+using Goblin.Domain.Entities;
+using Xunit;
+
+namespace Goblin.Application.Core.Tests.Commands.Merged
+{
+    public class WeatherDailyKeyboardCommandTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldReturnSuccessfulResult()
+        {
+            var command = new WeatherDailyKeyboardCommand();
+            var message = GenerateMessage(DefaultUser.Id, DefaultUser.Id, command.Aliases[0]);
+
+            var result = await command.Execute<VkBotUser>(message, DefaultUser);
+            result.Should().BeOfType<SuccessfulResult>();
+            result.Message.Should().NotBeNullOrEmpty();
+            result.Keyboard.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task ShouldReturnFailedResult_Because_UserCityIsEmpty()
+        {
+            DefaultUser.SetCity(string.Empty);
+            var command = new WeatherDailyKeyboardCommand();
+            var message = GenerateMessage(DefaultUser.Id, DefaultUser.Id, command.Aliases[0]);
+
+            var result = await command.Execute<VkBotUser>(message, DefaultUser);
+            result.Should().BeOfType<FailedResult>();
+            result.Message.Should().NotBeNullOrEmpty();
+        }
+    }
+}
