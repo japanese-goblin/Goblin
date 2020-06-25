56b52b42374e7b9c1ac850ab1334479fa3304310
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-25 11:23:19 +0300

    Добавить тесты для CommandsService

---
+++ tests/Goblin.Application.Core.Tests/Models/FakeAdminCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Goblin.Application.Core.Abstractions;
+using Goblin.Application.Core.Results.Success;
+using Goblin.Domain.Abstractions;
+
+namespace Goblin.Application.Core.Tests.Models
+{
+    public class FakeAdminCommand : ITextCommand
+    {
+        public bool IsAdminCommand => true;
+        public string[] Aliases => new[] { "demo" };
+
+        public Task<IResult> Execute<T>(IMessage msg, BotUser user) where T : BotUser
+        {
+            return Task.FromResult<IResult>(new SuccessfulResult
+            {
+                Message = "demo"
+            });
+        }
+    }
+}
