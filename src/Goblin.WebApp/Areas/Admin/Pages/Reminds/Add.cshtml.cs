4166ebb5f65a913962f104f0cf651130d5ab6fc8
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-25 12:37:23 +0300

    Переделать напоминалки на Razor pages

---
+++ src/Goblin.WebApp/Areas/Admin/Pages/Reminds/Add.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Goblin.DataAccess;
+using Goblin.Domain;
+using Goblin.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Serilog;
+
+namespace Goblin.WebApp.Areas.Admin.Pages.Reminds
+{
+    [Authorize(Roles = "Admin")]
+    [Area("Admin")]
+    public class Add : PageModel
+    {
+        public string ErrorMessage { get; set; }
+        private readonly BotDbContext _context;
+
+        public Add(BotDbContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost(long peerId, ConsumerType consumerType, string text, string dateStr, string timeStr)
+        {
+            var isCorrectDate = DateTime.TryParse($"{dateStr} {timeStr}", out var date);
+            if(!isCorrectDate)
+            {
+                ErrorMessage = "Некорректная дата";
+                return Page();
+            }
+
+            try
+            {
+                await _context.Reminds.AddAsync(new Remind(peerId, text, date, consumerType));
+                await _context.SaveChangesAsync();
+
+                return RedirectToPage("Index", "Reminds");
+            }
+            catch(Exception ex)
+            {
+                ErrorMessage = ex.Message;
+                Log.ForContext<Add>().Error(ex, "Невозможно добавить напоминание");
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ src/Goblin.WebApp/Areas/Admin/Pages/Reminds/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Goblin.DataAccess;
+using Goblin.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Goblin.WebApp.Areas.Admin.Pages.Reminds
+{
+    [Authorize(Roles = "Admin")]
+    [Area("Admin")]
+    public class Index : PageModel
+    {
+        public Remind[] Reminds { get; set; }
+        private readonly BotDbContext _context;
+
+        public Index(BotDbContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+            Reminds = _context.Reminds.ToArray();
+            ViewData["count"] = Reminds.Length;
+        }
+    }
+}
