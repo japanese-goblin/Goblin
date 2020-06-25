f26f135cc56df76e6472c063c39c1436fcdc6f9f
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-06-25 10:39:07 +0300

    Добавить тесты для StartCommand

---
+++ tests/Goblin.Application.Core.Tests/Commands/Merged/StartCommandTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Goblin.Application.Core.Commands.Merged;
+using Goblin.Application.Core.Results.Success;
+using Goblin.Domain.Entities;
+using Xunit;
+
+namespace Goblin.Application.Core.Tests.Commands.Merged
+{
+    public class StartCommandTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldReturnSuccessfulResult()
+        {
+            var command = new StartCommand();
+            var message = GenerateMessage(DefaultUser.Id, DefaultUser.Id, command.Aliases[0]);
+
+            var result = await command.Execute<VkBotUser>(message, DefaultUser);
+            result.Should().BeOfType<SuccessfulResult>();
+            result.Message.Should().NotBeNullOrEmpty();
+            result.Keyboard.Should().NotBeNull();
+        }
+    }
+}
