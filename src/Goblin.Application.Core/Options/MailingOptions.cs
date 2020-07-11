1fa3ff47b8673f87d33146d5c9c566c16037f0cf
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-07-11 12:02:02 +0300

    Добавить параметр IsVacations в MailingOptions

--- src/Goblin.Application.Core/Options/MailingOptions.cs
+++ src/Goblin.Application.Core/Options/MailingOptions.cs
@@ -2,6 +2,7 @@
 {
     public class MailingOptions
     {
+        public bool IsVacations { get; set; }
         public MailingSettings Schedule { get; set; }
         public MailingSettings Weather { get; set; }
     }
