f1919c154bf74849fc83833e7e6c1cf9bacc9c38
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-08-15 22:48:10 +0300

    Добавить страницу калькулятора

---
+++ src/Goblin.WebApp/Pages/Home/Calculator.cshtml.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Goblin.WebApp.Pages.Home
+{
+    public class Calculator : PageModel
+    {
+    }
+}
---
+++ src/Goblin.WebApp/wwwroot/js/calc.js
@@ -0,0 +1,37 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        firstItem: {
+            price: 0,
+            weight: 0
+        },
+        secondItem: {
+            price: 0,
+            weight: 0
+        },
+    },
+    computed: {
+        firstPricePerWeight: function () {
+            let res = +this.firstItem.price / +this.firstItem.weight;
+            return res.toFixed(2);
+        },
+        secondPricePerWeight: function () {
+            let res = +this.secondItem.price / +this.secondItem.weight;
+            return res.toFixed(2);
+        },
+        profit: function () {
+            let first = +this.firstPricePerWeight;
+            let second = +this.secondPricePerWeight;
+
+            let best = Math.max(first, second);
+            let worse = Math.min(first, second);
+
+            let stuff = first < second ? this.firstItem : this.secondItem;
+
+            let percent = Math.abs((worse - best) / best * 100).toFixed();
+
+            let selectedStuff = stuff === this.firstItem ? "Первый" : "Второй";
+            return `${selectedStuff} вариант выгоднее на ${percent}%`;
+        }
+    }
+})
