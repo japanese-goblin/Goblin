30aff2673f911a88ea7064d2ffcffac1ce1d1bb9
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2022-04-10 21:34:05 +0300

    Fix backend order

--- src/backend/Goblin.WebApp/Endpoints/Schedule/Get.cs
+++ src/backend/Goblin.WebApp/Endpoints/Schedule/Get.cs
@@ -23,13 +23,26 @@ public class Get : Endpoint<ScheduleRequest, ScheduleResponse>
     public override async Task HandleAsync(ScheduleRequest req, CancellationToken ct)
     {
         var r = await _narfuApi.Students.GetSchedule(req.GroupId, req.Date);
+        var dict = r.OrderBy(x => x.StartTime.Date)
+                          .ThenBy(x => x.Number)
+                          .GroupBy(x => GetStartOfWeek(x.StartTime));
         await SendAsync(new ScheduleResponse
         {
-            Lessons = r.GroupBy(x => x.StartTime.Date)
-                       .ToDictionary(x => x.Key, x => x.AsEnumerable()),
+            Lessons = dict.ToDictionary(x =>$"{x.Key:dd.MM.yyyy} - {x.Key.AddDays(5):dd.MM.yyyy}",
+                                        x => x.GroupBy(y => y.StartTime.Date)
+                                              .ToDictionary(y => y.Key.ToString("dddd, dd MMMM"),
+                                                            y => y.AsEnumerable())),
             GroupId = req.GroupId,
             GroupName = _narfuApi.Students.GetGroupByRealId(req.GroupId).Name
         });
+    }
+    
+    private static DateTime GetStartOfWeek(DateTime dt)
+    {
+        const DayOfWeek startOfWeek = DayOfWeek.Monday;
+        
+        var diff = (7 + (dt.DayOfWeek - startOfWeek)) % 7;
+        return dt.AddDays(-1 * diff).Date;
     }
 }
 
@@ -46,7 +59,7 @@ public class ScheduleResponse
 {
     public string GroupName { get; set; }
     public int GroupId { get; set; }
-    public Dictionary<DateTime, IEnumerable<Lesson>> Lessons { get; set; }
+    public Dictionary<string, Dictionary<string, IEnumerable<Lesson>>> Lessons { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
 }
