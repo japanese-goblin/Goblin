915f3935cee479ac71dac5e7cee360ed3948ad7b
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2022-05-09 19:55:02 +0300

    Add roles endpoint

--- src/backend/Goblin.WebApp/Endpoints/Auth/Auth.Check.cs
+++ src/backend/Goblin.WebApp/Endpoints/Auth/Auth.Check.cs
@@ -1,9 +1,18 @@
-﻿using FastEndpoints;
+﻿using System.Security.Claims;
+using FastEndpoints;
+using Microsoft.AspNetCore.Identity;
 
 namespace Goblin.WebApp.Endpoints.Auth;
 
 public class AuthCheckEndpoint : Endpoint<AuthEndpointRequest>
 {
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AuthCheckEndpoint(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+    
     public override void Configure()
     {
         Get("/auth/check");
@@ -12,6 +21,6 @@ public class AuthCheckEndpoint : Endpoint<AuthEndpointRequest>
 
     public override Task HandleAsync(AuthEndpointRequest req, CancellationToken ct)
     {
-        return SendOkAsync(ct);
+        return SendOkAsync(User.FindAll(ClaimTypes.Role).Select(x => x.Value), ct);
     }
 }
