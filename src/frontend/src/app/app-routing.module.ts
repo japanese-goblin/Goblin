ced3f9a8c6c53418b2f6822e247f1af0faf9a3d5
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2022-08-26 17:59:50 +0300

    Добавить изменение названия страницы в роутах

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -1,14 +1,24 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {Injectable, NgModule} from '@angular/core';
+import {ActivatedRouteSnapshot, RouterModule, Routes} from '@angular/router';
 import {ScheduleDetailsComponent} from './schedule-details/schedule-details.component';
 import {ScheduleComponent} from './schedule/schedule.component';
 import {StartComponent} from './start/start.component';
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
+@Injectable({ providedIn: 'root' })
+export class GroupTitleResolver {
+    resolve(route: ActivatedRouteSnapshot) {
+        const userId = route.paramMap.get('groupId');
+        console.log(route.paramMap)
+        return `Расписание ${userId} - Японский Гоблин`;
+    }
+}
+
 const appRoutes: Routes = [
     {
         path: '',
-        component: StartComponent
+        component: StartComponent,
+        title: "Японский гоблин"
     },
     {
         path: 'schedule',
@@ -16,13 +26,16 @@ const appRoutes: Routes = [
         children: [
             {
                 path: ':groupId',
-                component: ScheduleDetailsComponent
+                component: ScheduleDetailsComponent,
+                title: GroupTitleResolver
             }
-        ]
+        ],
+        title: "Расписание - Японский гоблин"
     },
     {
         path: '**',
-        component: PageNotFoundComponent
+        component: PageNotFoundComponent,
+        title: "404 - Японский гоблин"
     }
 ];
 
