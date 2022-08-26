25621e241ee0695a4e073c8509e31c3e84ed393f
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2022-08-26 17:54:25 +0300

    Обновить angular до 14.2

--- src/frontend/src/app/schedule/schedule.component.ts
+++ src/frontend/src/app/schedule/schedule.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+import {UntypedFormBuilder} from '@angular/forms';
 import {Router} from '@angular/router';
 
 
@@ -17,7 +17,7 @@ export class ScheduleComponent implements OnInit {
         date: undefined
     });
 
-    constructor(private formBuilder: FormBuilder, private router: Router) {
+    constructor(private formBuilder: UntypedFormBuilder, private router: Router) {
         const thisYear = new Date().getFullYear();
         this.minDate = new Date(thisYear - 1, 8, 1);
         this.router.routeReuseStrategy.shouldReuseRoute = () => false;
