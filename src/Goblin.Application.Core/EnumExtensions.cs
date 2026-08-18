a11c2d9d72f1ce0dad66a077053b2114f0f6e33e
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2026-08-18 12:10:58 +0300

    Add EnumExtensions

---
+++ src/Goblin.Application.Core/EnumExtensions.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace Goblin.Application.Core;
+
+internal static class EnumExtensions
+{
+    public static string GetEnumMemberValue<TEnum>(this TEnum value)
+        where TEnum : struct, Enum
+    {
+        var member = typeof(TEnum)
+            .GetMember(value.ToString())
+            .FirstOrDefault();
+
+        return member?.GetCustomAttribute<EnumMemberAttribute>()?.Value
+               ?? value.ToString();
+    }
+}
